8312346fbd236f075e25ff66c8a5e5ad8d6d831e
Amr Riyad <74285642+Achilles404@users.noreply.github.com>
2022-07-04 16:00:15 +0200

    Create main.cpp

---
+++ B. At Least One Prime Divisor/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define Achilles ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+
+void Achilles1() {
+    ll n;
+    cin >> n;
+    if (n == 1) {
+        cout << "NO";
+    } else {
+        cout << "YES";
+    }
+}
+
+//XD
+
+int main() {
+    Achilles
+    int tt = 1;
+//    cin >> tt;
+    while (tt--) Achilles1();
+}
